7c30058f5a429552045e352898872fdfda37bf9e
SuperEthan <ysh6161@126.com>
2024-09-23 12:20:34 -0400

    update R file

--- HW4/QuestionA.R
+++ HW4/QuestionA.R
@@ -1,12 +1,22 @@
 library(data.table)
 file_root<-"https://www.ndbc.noaa.gov/view_text_file.php?filename=44013h"
-years<-1985:2023
+years_skip1<-1985:1999
 df_list <- list() # create an empty list to store data
 tail<- ".txt.gz&dir=data/historical/stdmet/"
-for (year in years){
-  path<-paste0(file_root,year,tail)
+for (year in years_skip1){
+  path<-paste0(file_root,year,tail) # combine and create the whole URL
+  # read the URL and use the first line as header
   header<-scan(path,what= 'character',nlines=1)
-  buoy<-fread(path,header=FALSE,skip=2)
+  # read the actual data and do not treat the first row as header
+  buoy<-fread(path,header=FALSE,skip=1, fill=TRUE)
+  # manually assign the header
   colnames(buoy)<-header
+  df_list[[as.character(year)]] <- buoy
 }
+print(head(df_list))
+# Combine all data into a single data.table
+all_data <- rbindlist(df_list, use.names = TRUE, fill = TRUE)
+
+# View the combined data
+print(head(all_data))
 
