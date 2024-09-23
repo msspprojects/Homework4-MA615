3a7527df375d81b1396b368f1da65ede0b0b2aeb
SuperEthan <ysh6161@126.com>
2024-09-23 19:02:49 -0400

    add functions;

--- HW4/QuestionA.R
+++ HW4/QuestionA.R
@@ -1,22 +1,54 @@
 library(data.table)
+library(stringr)
 file_root<-"https://www.ndbc.noaa.gov/view_text_file.php?filename=44013h"
-years_skip1<-1985:1999
+years<-1985:2023
 df_list <- list() # create an empty list to store data
 tail<- ".txt.gz&dir=data/historical/stdmet/"
-for (year in years_skip1){
+all_columns <- c("YY", "MM", "DD", "hh", "mm", "WDIR", "WSPD", "GST", "WVHT", 
+                 "DPD", "APD", "MWD", "PRES", "ATMP", "WTMP", "DEWP", "VIS", 
+                 "TIDE")
+# Function to determine lines to skip
+lines_to_skip <- function(path){
+  content <- file(path, open = "r") # read the file
+  first3 <- readLines(content, n = 2) # read the first three lines
+  close(content)
+  
+  # check if the second line are all numbers
+  second_line <- str_split(first3[2], "\\s+", simplify = TRUE)
+  check_numeric <- str_detect(second_line, "^-?\\d*\\.?\\d+$")
+  
+  if (all(check_numeric)){
+    return(1) # second line all numeric, only first line is header
+  } else{
+    return(2) # first and second lines are header
+  }
+}
+
+# Function to read data in each file
+read_file <- function(year){
   path<-paste0(file_root,year,tail) # combine and create the whole URL
-  # read the URL and use the first line as header
-  header<-scan(path,what= 'character',nlines=1)
-  # read the actual data and do not treat the first row as header
-  buoy<-fread(path,header=FALSE,skip=1, fill=TRUE)
+  header<-scan(path,what='character',nlines=1)
+  skip_line <- lines_to_skip(path) # determine how many lines to skip
+  buoy<-fread(path,header=FALSE,skip=skip_line, fill=TRUE)
   # manually assign the header
   colnames(buoy)<-header
-  df_list[[as.character(year)]] <- buoy
+  col_miss <- setdiff(all_columns, names(buoy))
+  for (col in col_miss){
+    set(buoy, j=col, value=NA)
+  }
+  setcolorder(buoy, all_columns)
+  return(buoy)
 }
-print(head(df_list))
-# Combine all data into a single data.table
-all_data <- rbindlist(df_list, use.names = TRUE, fill = TRUE)
 
-# View the combined data
-print(head(all_data))
+
+for (year in years) {
+  data <- read_file(year)
+  df_list[[as.character(year)]] <- data
+}
+
+# Combine all the datasets into one data.table
+combined_data <- rbindlist(df_list, use.names = TRUE, fill = TRUE)
+
+# View the first few rows of the combined dataset
+print(head(combined_data))
 
