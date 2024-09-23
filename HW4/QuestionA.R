5a5c9174ffeba7bd73a1090da8468537a22466c0
Ethan Yao <yshgooole@gmail.com>
2024-09-23 09:34:07 -0400

    change r file;

--- HW4/QuestionA.R
+++ HW4/QuestionA.R
@@ -1,9 +1,12 @@
 library(data.table)
 file_root<-"https://www.ndbc.noaa.gov/view_text_file.php?filename=44013h"
 years<-1985:2023
-df_list <- list()
+df_list <- list() # create an empty list to store data
 tail<- ".txt.gz&dir=data/historical/stdmet/"
-path<-paste0(file_root,year,tail)
-header<-scan(path,what= 'character',nlines=1)
-buoy<-fread(path,header=FALSE,skip=2)
-colnames(buoy)<-header
+for (year in years){
+  path<-paste0(file_root,year,tail)
+  header<-scan(path,what= 'character',nlines=1)
+  buoy<-fread(path,header=FALSE,skip=2)
+  colnames(buoy)<-header
+}
+
